61eb9b652560ba8ebd09e69dc26030fccfcc456a
makfluffy575 <fluffy.mk575@gmail.com>
2025-02-22 15:23:17 +0300

    new version numbers

--- numbers/index.js
+++ numbers/index.js
@@ -2,15 +2,13 @@
 let numbers = [];
 
 //заполняем массив целыми числами
-for (let n=-10; n<=10; n+=1)
-{
+for (let n=-10; n<=10; n+=1) {
   numbers.push(n);
 }
 // console.log(numbers);
 
 
-for (let i=0; i<numbers.length; i+=1)
-{
+for (let i=0; i<numbers.length; i+=1) {
   //удаляем из массива отрицательные числа
   while (numbers[i] < 0) {
     numbers.shift();
@@ -18,15 +16,45 @@ for (let i=0; i<numbers.length; i+=1)
   
   //возводим числа в квадрат
   numbers[i] = numbers[i] ** 2;
-  
 }
 
-//вывод чисел в порядке убывания
-const numbersReverse = [];
-for (let i = (numbers.length -1); i>=0; i--) {
-  numbersReverse.push(numbers[i]);
+//сортировка в порядке убывания
+for (let i = numbers.length - 1; i > 0; i -= 1) {
+  let indexMax = i;
+  for (let j = i - 1; j >= 0; j -= 1) {
+    if (numbers[j] < numbers[indexMax]) {
+      indexMax = j;
+    }
+  }
+const x = numbers[i];
+numbers[i] = numbers[indexMax];
+numbers[indexMax] = x;
 }
-console.log(numbersReverse);
+
+console.log(numbers);
+
+
+//вывод чисел в порядке убывания (реверс) - 1й вариант
+// const numbersReverse = [];
+// for (let i = (numbers.length - 1); i >= 0; i--) {
+//   numbersReverse.push(numbers[i]);
+// }
+// console.log(numbersReverse);
+
+
+//вывод чисел в порядке убывания (реверс) - 2й вариант
+// let left = 0;
+// let right = numbers.length - 1;
+
+// while (left < right) {
+//   const x = numbers[left];
+//   numbers[left] = numbers[right];
+//   numbers[right] = x;
+
+//   left += 1;
+//   right -= 1;
+// }
+// console.log(numbers);
 
 
 //вывод чисел в порядке убывания без цикла
